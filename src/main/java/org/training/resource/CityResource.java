28230b24c98e2a4792a048e5097bf6a607c6e9f4
qtpeters <qtpeters>
2014-08-24 22:10:55 -0400

    Got Jersey working: RESTful services

--- src/main/java/org/training/resource/CityResource.java
+++ src/main/java/org/training/resource/CityResource.java
@@ -1,5 +1,18 @@
 package org.training.resource;
 
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("city")
 public class CityResource {
 
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String topLevelCity() {
+		return "Example!";
+	}
+	
 }
