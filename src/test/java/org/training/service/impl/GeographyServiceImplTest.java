fd7cf230b30adf9be38847dbfd05beac01ef53af
qtpeters <qtpeters>
2014-09-21 20:42:22 -0400

    New SqlUtil, refactored and cleaned up Unit test.

--- src/test/java/org/training/service/impl/GeographyServiceImplTest.java
+++ src/test/java/org/training/service/impl/GeographyServiceImplTest.java
@@ -11,24 +11,35 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.training.model.City;
+import org.training.service.util.SqlUtil;
 
 import static org.junit.Assert.*;
 
 public class GeographyServiceImplTest {
 
+	private Connection conn;
 	private GeographyServiceImpl service;
 	
 	@Before
 	public void init() {
 		this.service = new GeographyServiceImpl();
 		this.service.init();
+		try {
+			this.conn = DriverManager.getConnection( "jdbc:mysql://localhost/dqmc?user=dqmc&password=dqmc123" );
+		} catch (SQLException e) {
+			throw new ExceptionInInitializerError( 
+					"Problem initalizing GeographyService tests: " + e.getMessage() );
+		} finally {
+			SqlUtil.close( conn );
+		}
 	}
 	
 	@After
 	public void destroy() {
+		SqlUtil.close( conn );
 		if ( this.service != null ) {
 			this.service.destroy();
-		}
+		}		
 	}
 	
 	@Test
@@ -39,34 +50,22 @@ public class GeographyServiceImplTest {
 		City city = new City();
 		city.setName( uuid );
 		this.service.addCity(city);
-		
-		Connection conn = null;
-		try {
-			conn = DriverManager.getConnection( "jdbc:mysql://localhost/dqmc?user=dqmc&password=dqmc123" );
-			PreparedStatement ps = conn.prepareStatement( "SELECT * FROM city WHERE name= ('" + uuid + "')" );
-			ResultSet rs = ps.executeQuery();
-			if ( ! rs.first() ) {				
-				fail( "No valid rows in ResultSet, Hibernate failed." );
-			} else {
-				String uuidFromDb = rs.getString("name"); 
-				assertTrue( uuid + " :NOT EQUAL TO: " + uuidFromDb, 
-						uuidFromDb.equals( uuid ) );
-				this.service.removeCity( city );
-			}
-		} catch ( SQLException e ) {
-			e.printStackTrace();
-		} finally {
-			close( conn );
+
+		ResultSet rs = SqlUtil.executeSqlQuery( "SELECT * FROM city WHERE name= ('" + uuid + "')", conn );
+
+		if ( ! SqlUtil.hasResult( rs ) ) {				
+			fail( "No valid rows in ResultSet, Hibernate failed." );
+		} else {
+			String uuidFromDb = SqlUtil.getString( rs, "name"); 
+			assertTrue( uuid + " :NOT EQUAL TO: " + uuidFromDb, 
+					uuidFromDb.equals( uuid ) );
+			this.service.removeCity( city );
 		}
 	}
 
-	private void close( Connection conn ) {
-		try {
-			if ( conn != null ) {
-				conn.close();
-			}
-		} catch ( SQLException se ) {
-			System.err.println( "Unresolvable SQL Connection error." );
-		}
+	@Test
+	public void removeServiceTest() {
+		
 	}
+
 }
---
+++ src/test/java/org/training/service/util/SqlUtil.java
@@ -0,0 +1,58 @@
+package org.training.service.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public final class SqlUtil {
+	
+	public static String getString( ResultSet rs, String fieldName ) {
+		String result = null;
+		
+		try {
+			result = rs.getString( fieldName );
+		} catch ( SQLException se ) {
+			se.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public static boolean hasResult( ResultSet rs ) {
+		boolean hasRecord = false;
+		try {
+			hasRecord = rs.first();
+		} catch ( SQLException se ) {
+			se.printStackTrace();
+			hasRecord = false;
+		}
+		
+		return hasRecord;
+	}
+	
+	public static ResultSet executeSqlQuery( String sql, Connection conn ) {
+		
+		ResultSet rs = null;
+		try {
+			PreparedStatement ps = conn.prepareStatement( sql );
+			rs = ps.executeQuery();
+		} catch ( SQLException se ) {
+			se.printStackTrace();
+		} finally {
+			close( conn );
+		}
+		
+		return rs;
+	}
+	
+	public static void close( Connection conn ) {
+		try {
+			if ( conn != null ) {
+				conn.close();
+			}
+		} catch ( SQLException se ) {
+			System.err.println( "Unresolvable SQL Connection error." );
+		}
+	}
+}
